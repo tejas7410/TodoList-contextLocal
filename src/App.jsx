75e9586ecef54ee3f509915b3697907019d1c393
tejas7410 <img_2021030@iiitm.ac.in>
2025-05-14 18:47:32 +0530

    Updates README.md and UI of the app

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 
   return (
     <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
-      <div className="bg-[#172842] min-h-screen py-8 w-screen">
+      <div className="bg-[#172842] min-h-screen py-8 ">
         <div className="w-full max-w-2xl px-4 py-3 mx-auto text-white rounded-lg shadow-md">
           <h1 className="mt-2 mb-8 text-2xl font-bold text-center">Manage Your Todos</h1>
           <div className="mb-4">
